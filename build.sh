65b35b70d607052466874b20cf13c5d7679a956c
marcosantos98 <marcomsantos98@gmail.com>
2026-08-24 19:14:56 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "release" ]; then
-    odin build . -show-timings -vet -o:speed
+    odin build . -show-timings -vet -o:speed -error-pos-style:unix
 else
-    odin build . -show-timings -debug -vet
+    odin build . -show-timings -debug -vet -error-pos-style:unix
 fi
